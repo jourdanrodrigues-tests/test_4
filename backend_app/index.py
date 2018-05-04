5d50d5b775d3662484119cb7cd2c5168f43ef892
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-05-04 19:12:44 -0300

    :sparkles: Add new endpoints and new class for Response

--- backend_app/index.py
+++ backend_app/index.py
@@ -1,11 +1,14 @@
 import os
 import re
+from functools import reduce
 
+from bson import ObjectId
 from flask import Flask
+from flask import request
 from flask_cors import cross_origin
 from pymongo import MongoClient
 
-from .utils import get_collection, json_response
+from .utils import get_collection, Response
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 
@@ -14,14 +17,13 @@ if not DATABASE_URL:
 
 connection = MongoClient(DATABASE_URL)
 
-
 DATABASE_NAME = re.search(r'/(\w+)$', DATABASE_URL).group(1)
 songs_collection = get_collection(connection[DATABASE_NAME])
 
 app = Flask(__name__)
 
 
-@app.route('/api/songs/')
+@app.route('/api/songs/', methods=['GET'])
 @cross_origin()
 def songs():
     columns = {
@@ -33,5 +35,71 @@ def songs():
         'released': 1,
         'difficulty': 1,
     }
-    _songs = list(songs_collection.find({}, columns))
-    return json_response(_songs)
+    _songs = songs_collection.find({}, columns)
+    return Response(_songs).json()
+
+
+@app.route('/api/songs/search/', methods=['GET'])
+@cross_origin()
+def songs_search():
+    columns = {
+        '_id': 1,  # More explicit
+        'title': 1,
+        'level': 1,
+        'artist': 1,
+        'rating': 1,
+        'released': 1,
+        'difficulty': 1,
+    }
+    # Wondering why this is named "message" in the requirements
+    to_search = request.args.get('message')
+
+    if not to_search:
+        return Response([]).json()  # Saving a query
+
+    search_query = {'$regex': r'.*{}.*'.format(to_search)}
+    where = {
+        '$or': [
+            {'title': search_query},
+            {'artist': search_query},
+        ]
+    }
+
+    _songs = songs_collection.find(where, columns)
+
+    return Response(_songs).json()
+
+
+@app.route('/api/songs/avg/difficulty/', methods=['GET'])
+@cross_origin()
+def average_difficulty():
+    columns = {'_id': 0, 'difficulty': 1}
+
+    level = request.args.get('level')
+    where = {}
+
+    if level:
+        where['level'] = int(level)
+
+    _songs = list(map(lambda song: song['difficulty'], songs_collection.find(where, columns)))
+
+    average = round(reduce(lambda avg, value: avg + value, _songs) / len(_songs), 2) if _songs else 0
+
+    return Response({'average': average}).json()
+
+
+@app.route('/api/songs/rating/<int:song_id>/', methods=['POST'])
+@cross_origin()
+def set_rating(song_id):
+    where = {'_id': ObjectId(str(song_id))}
+    rating = request.json.get('rating')
+
+    if not rating:
+        return Response('Required "rating" not sent').bad_request()
+
+    query_result = songs_collection.update_one(where, {'$set': {'rating': rating}})
+
+    if not query_result.matched_count:
+        return Response('Song not found').not_found()
+
+    return Response.no_content()
--- backend_app/utils.py
+++ backend_app/utils.py
@@ -1,9 +1,10 @@
 import json
 import os
+from http import HTTPStatus
 
 from bson.json_util import dumps as mongo_dumps
 from flask import jsonify
-from flask.wrappers import Response
+from flask.wrappers import Response as FlaskResponse
 
 COLLECTION = 'songs'
 
@@ -25,6 +26,21 @@ def get_collection(db_conn):
     return collection
 
 
-def json_response(data: dict or list) -> Response:
-    # https://stackoverflow.com/a/27024423/4694834
-    return jsonify(json.loads(mongo_dumps(data)))
+class Response:
+    def __init__(self, data: str or dict or list):
+        self.data = data
+
+    def json(self, error=False) -> FlaskResponse:
+        data = {'message': self.data} if error else self.data
+        # https://stackoverflow.com/a/27024423/4694834
+        return jsonify(json.loads(mongo_dumps(data)))
+
+    def bad_request(self) -> tuple:
+        return self.json(), HTTPStatus.BAD_REQUEST
+
+    def not_found(self) -> tuple:
+        return self.json(), HTTPStatus.NOT_FOUND
+
+    @staticmethod
+    def no_content() -> tuple:
+        return '', HTTPStatus.NO_CONTENT
